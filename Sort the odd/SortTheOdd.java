ea70e9c85b6bfbca5124b404c97b9089d349eb98
choulli <46460763+othmaneChoulli@users.noreply.github.com>
2020-01-04 08:28:02 +0100

    Create SortTheOdd.java

---
+++ Sort the odd/SortTheOdd.java
@@ -0,0 +1,15 @@
+public class Kata {
+  public static int[] sortArray(int[] array) {
+  
+    for(int i=0 ; i<array.length-1 ; i++){
+      for(int j=i+1; j<array.length ; j++){
+        if(array[i]>array[j] && array[i]%2 == 1 && array[j]%2 == 1){
+          int aux=array[i];
+          array[i]=array[j];
+          array[j]=aux;
+        }
+       }
+      }
+    return array;
+  }
+}

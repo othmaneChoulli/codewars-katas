63b828fbeea3619b668c3fa6f89a008ed5110cbb
choulli <46460763+othmaneChoulli@users.noreply.github.com>
2020-01-06 05:40:48 +0100

    Create Solution.java

---
+++ Find the stray number/Solution.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+  static int stray(int[] numbers) {
+    Arrays.sort(numbers);
+    return numbers[0] == numbers[1] ? numbers[numbers.length-1] : numbers[0];
+  }
+}
